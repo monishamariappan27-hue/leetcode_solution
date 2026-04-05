bd6592609218052e2ed7a3571c27c067372d9a50
monishamariappan27-hue <monishamariappan27@gmail.com>
2026-04-05 08:50:36 +0530

    Time: 8 ms (58.93%), Space: 13.1 MB (96.26%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,23 @@
+from collections import deque
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for a,b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        queue = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        count = 0
+        while queue:
+            course = queue.popleft()
+            count += 1
+
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        return count == numCourses
