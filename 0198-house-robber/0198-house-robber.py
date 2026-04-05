bb0e33380a8b2ff04709180fb41b5adbf2f21e34
monishamariappan27-hue <monishamariappan27@gmail.com>
2026-04-05 09:16:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (93.43%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rob(self, nums):
+        prev2 = 0
+        prev1 = 0
+
+        for num in nums:
+            curr = max(num + prev2, prev1)
+            prev2 = prev1
+            prev1 = curr
+        return prev1
+        
